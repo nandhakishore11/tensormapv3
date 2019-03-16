3b46b9c987e420d454a0c353f9a102ccbcfb802d
Nandhakishore <nandhakishore11@gmail.com>
2019-03-16 08:49:06 +0530

    Updates

---
+++ precache-manifest.6cdb728d2d35c292f5e4ba7af788fd41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tensormapv3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34a408a43762a2738686",
+    "url": "/tensormapv3/static/js/runtime~main.b1c39d63.js"
+  },
+  {
+    "revision": "8861783489668ba8ca0b",
+    "url": "/tensormapv3/static/js/main.4b0a5974.chunk.js"
+  },
+  {
+    "revision": "4049fb3d78991417cf58",
+    "url": "/tensormapv3/static/js/2.41bf4a61.chunk.js"
+  },
+  {
+    "revision": "8861783489668ba8ca0b",
+    "url": "/tensormapv3/static/css/main.335eb7d8.chunk.css"
+  },
+  {
+    "revision": "16c6e05c36b6198ead75d52cd9660da2",
+    "url": "/tensormapv3/index.html"
+  }
+];
